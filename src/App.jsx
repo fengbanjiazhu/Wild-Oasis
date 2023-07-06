7cf5a74f24960daebf961083d2e89e2496e7d453
Jeffrey <562475805lanu@gmail.com>
2023-07-06 18:31:10 +1000

    test styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,54 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  margin: 20px;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <>Hi</>;
+  return (
+    <StyledApp>
+      <H1>The Wild Oasis</H1>
+      <Button
+        onClick={() => {
+          console.log("checked in");
+        }}
+      >
+        Check in
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("checked out");
+        }}
+      >
+        Check out
+      </Button>
+      <Input type="number" placeholder="Number of guest"></Input>
+    </StyledApp>
+  );
 }
 
 export default App;
