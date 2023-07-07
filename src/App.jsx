7938c31565bd17e8c3444aca0f6fd5b434487644
Jeffrey <562475805lanu@gmail.com>
2023-07-07 18:18:21 +1000

    build reusable styled component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -14,24 +15,38 @@ function App() {
     <>
       <GlobalStyles></GlobalStyles>
       <StyledApp>
-        <Heading type="h1">The Wild Oasis</Heading>
-        <Heading type="h2">Check in and out</Heading>
-        <Button
-          onClick={() => {
-            console.log("checked in");
-          }}
-        >
-          Check in
-        </Button>
-        <Button
-          onClick={() => {
-            console.log("checked out");
-          }}
-        >
-          Check out
-        </Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guest"></Input>
+        <Row>
+          <Row type="horizontal">
+            <Heading type="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading type="h2">Check in and out</Heading>
+              <Button
+                onClick={() => {
+                  console.log("checked in");
+                }}
+              >
+                Check in
+              </Button>
+              <Button
+                size="small"
+                variation="secondary"
+                onClick={() => {
+                  console.log("checked out");
+                }}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guest"></Input>
+              <Input type="number" placeholder="Number of guest"></Input>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
