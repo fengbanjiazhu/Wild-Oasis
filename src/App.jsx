85d4eaecbe4f054a73e487858fde45803965bae3
Jeffrey <562475805lanu@gmail.com>
2023-07-08 17:52:41 +1000

    implement route

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,31 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.div`
-  /* background-color: orangered; */
-  padding: 20px;
-`;
 
 function App() {
   return (
     <>
       <GlobalStyles></GlobalStyles>
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading type="h1">The Wild Oasis</Heading>
-            <div>
-              <Heading type="h2">Check in and out</Heading>
-              <Button
-                onClick={() => {
-                  console.log("checked in");
-                }}
-              >
-                Check in
-              </Button>
-              <Button
-                size="small"
-                variation="secondary"
-                onClick={() => {
-                  console.log("checked out");
-                }}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of guest"></Input>
-              <Input type="number" placeholder="Number of guest"></Input>
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="/dashboard" />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/cabins" element={<Cabins />}></Route>
+          <Route path="/bookings" element={<Bookings />}></Route>
+          <Route path="/users" element={<Users />}></Route>
+          <Route path="/settings" element={<Settings />}></Route>
+          <Route path="/account" element={<Account />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
