4cc862b7e1831127f7bf6dc98d5b2b8ff9e1031d
Jeffrey <562475805lanu@gmail.com>
2023-07-17 11:32:17 +1000

    add more layout with antd

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,26 +4,40 @@ import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal(!isOpenModal)}> Add new cabin</Button>
-      {isOpenModal && (
-        <Modal
-          onCloseModal={() => {
-            setIsOpenModal(false);
-          }}
-        >
-          <CreateCabinForm
-            onCloseModal={() => {
-              setIsOpenModal(false);
-            }}
-          ></CreateCabinForm>
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open>
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+
+      <Modal.Window>
+        <CreateCabinForm></CreateCabinForm>
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal(!isOpenModal)}> Add new cabin</Button>
+//       {isOpenModal && (
+//         <Modal
+//           onCloseModal={() => {
+//             setIsOpenModal(false);
+//           }}
+//         >
+//           <CreateCabinForm
+//             onCloseModal={() => {
+//               setIsOpenModal(false);
+//             }}
+//           ></CreateCabinForm>
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
