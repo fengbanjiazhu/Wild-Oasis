f1fc31c41cf0b482b7cb74794fb20234f34d2d9c
Jeffrey <562475805lanu@gmail.com>
2023-07-19 18:32:05 +1000

    rebuild a new table row with compound component

--- src/features/cabins/CabinTable-v1.jsx
+++ src/features/cabins/CabinTable-v1.jsx
@@ -4,7 +4,15 @@ import Spinner from "../../ui/Spinner";
 
 import styled from "styled-components";
 import CabinRow from "./CabinRow";
-import Table from "../../ui/Table";
+
+const Table = styled.div`
+  border: 1px solid var(--color-grey-200);
+
+  font-size: 1.4rem;
+  background-color: var(--color-grey-0);
+  border-radius: 7px;
+  overflow: hidden;
+`;
 
 const TableHeader = styled.header`
   display: grid;
@@ -27,15 +35,15 @@ function CabinTable() {
   if (isLoading) return <Spinner></Spinner>;
 
   return (
-    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-      <Table.Header>
+    <Table role="table">
+      <TableHeader role="row">
         <div></div>
         <div>Cabin</div>
         <div>Capacity</div>
         <div>Price</div>
         <div>Discount</div>
         <div></div>
-      </Table.Header>
+      </TableHeader>
       {cabins.map((cabin) => (
         <CabinRow key={cabin.id} cabin={cabin} />
       ))}
