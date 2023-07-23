bad7f560aac0cf4fc8ee179aaa0a4b062b8455fb
Jeffrey <562475805lanu@gmail.com>
2023-07-23 17:58:30 +1000

    add server side data sorting

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -7,7 +7,10 @@ export async function getBookings({ filter, sortBy }) {
   // .gte("totalPrice", 5000);
 
   // filter
-  if (filter !== null) query = query[filter.method || "eq"](filter.field, filter.value);
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  // sort
+  if (sortBy) query = query.order(sortBy.field, { ascending: sortBy.direction === "asc" });
 
   const { data, error } = await query;
 
