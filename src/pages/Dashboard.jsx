7772a67b17ce2a6e7807474928128cf5bb1907c3
Jeffrey <562475805lanu@gmail.com>
2023-08-05 11:55:51 +1000

    add hooks to filter recent booking

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,8 +2,16 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import DashboardLayout from "../features/dashboard/DashboardLayout";
 import DashboardFilter from "../features/dashboard/DashboardFilter";
+import { useRecentBookings } from "../features/dashboard/useRecentBookings";
+import Spinner from "../ui/Spinner";
+import { useRecentStays } from "../features/dashboard/useRecentStays";
 
 function Dashboard() {
+  const { bookings, isLoading } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoadingStays } = useRecentStays();
+
+  if (isLoading || isLoadingStays) return <Spinner />;
+
   return (
     <>
       <Row type="horizontal">
